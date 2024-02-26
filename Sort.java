3b82a21c0ba3dce77f453724bf4795326d116f5e
Anjaliev <133093479+Anjaliev@users.noreply.github.com>
2024-02-26 11:59:14 +0530

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Sort
+{
+public static void main(String[] args)
+{
+System.out.println("Anjali E v 23MCA016 26/02/24\n");
+String[] fruit={"Apple","Orange","Banana","Graps"};
+System.out.println("Array is:"+Arrays.toString(fruit));
+Arrays.sort(fruit);
+System.out.println("Sorted array is:"+Arrays.toString(fruit));
+}
+}
