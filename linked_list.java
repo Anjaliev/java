a0dd9451f1615d8746799aa0748ebbdf9574d309
Anjaliev <133093479+Anjaliev@users.noreply.github.com>
2024-04-15 11:31:21 +0530

    Add files via upload

---
+++ linked_list.java
@@ -0,0 +1,22 @@
+//4.11
+import java.util.*;
+public class linked_list {
+public static void main(String[] args){
+System.out.println("Anjali E V(23MCA016)\n15/04/2024");
+System.out.println();
+LinkedList<String> L=new LinkedList<>();
+L.add("Gold");
+L.add("Silver");
+L.add("Bronze");
+L.add(0,"Olympics Medals");
+System.out.println(L);
+L.remove("Bronze");
+System.out.println(L);
+L.remove(2);
+System.out.println(L);
+L.removeLast();
+System.out.println(L);
+L.removeFirst();
+System.out.println(L);
+}
+}
